5989ee8abf0c20759b7e1b59d9ce7ea6cc3f8f1e
fernando <s24hseguridad@gmail.com>
2024-08-17 23:43:09 -0500

    7

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Camara  # Reemplaza 'Camara' con el nombre de tu modelo
 
-# Register your models here.
+@admin.register(Camara)
+class CamaraAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'modelo', 'numero_serie', 'ubicacion', 'fecha_instalacion')
