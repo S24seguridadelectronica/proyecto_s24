5cfb4fd92ebe723b209a7dc817224e31f62fd1d0
fernando <s24hseguridad@gmail.com>
2024-08-18 02:12:28 -0500

    14

--- inventario/models.py
+++ inventario/models.py
@@ -1,11 +1,13 @@
+# cameras/models.py
+
 from django.db import models
 
 class Camara(models.Model):
     nombre = models.CharField(max_length=100)
     modelo = models.CharField(max_length=100)
     numero_serie = models.CharField(max_length=100, unique=True)
-    ubicacion = models.CharField(max_length=100)
+    ubicacion = models.CharField(max_length=255)
     fecha_instalacion = models.DateField()
 
     def __str__(self):
-        return f"{self.nombre} - {self.modelo}"
+        return self.nombre
