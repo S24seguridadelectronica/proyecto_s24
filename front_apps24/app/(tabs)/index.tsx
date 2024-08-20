1de562c9a38607c9571f38cd54865b54158e615b
fernando <s24hseguridad@gmail.com>
2024-08-20 01:30:28 -0500

    7

--- front_apps24/app/(tabs)/index.tsx
+++ front_apps24/app/(tabs)/index.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import CamarasScreen from './CamarasScreen';
+import CamarasScreen from './CamarasScreen';  // Importa tu nuevo componente
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
     return (
-        <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Cámaras" component={CamarasScreen} />
-                {/* Otras pantallas */}
-            </Tab.Navigator>
-        </NavigationContainer>
+        <Tab.Navigator>
+            <Tab.Screen name="Cámaras" component={CamarasScreen} />
+            {/* Otras pantallas */}
+        </Tab.Navigator>
     );
 };
 
